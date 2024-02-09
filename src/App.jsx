2ed7bf531efc6e6b32831228555cf71f52bab940
alanchibilisco <alan.chibilisco@gmail.com>
2024-02-08 21:30:21 -0300

    se crearon navbar y footer y lista de tareas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import Footer from "./components/Footer/Footer"
+import Navbarr from "./components/Navbar/Navbar"
+import Tareas from "./components/Tareas/Tareas"
 
 function App() {
   
   return (
-    <>     
+    <>   
+
+      <Navbarr/> 
+      <Tareas/>
+      <Footer/>
     </>
   )
 }
